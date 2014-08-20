5d95f658951f502ec17e8c450ff969440e0d8329
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-08-20 00:11:30 -0700

    Example of more complex audio synthesis

--- game/scene1.js
+++ game/scene1.js
@@ -8,17 +8,25 @@ exports.prototype.tick = function() {
 
 exports.prototype.init = function() {
 	var synth = flock.synth({
-	    synthDef: {
-	        id: "carrier",
-	        ugen: "flock.ugen.sinOsc",
-	        freq: 440,
-	        mul: {
-	            id: "mod",
-	            ugen: "flock.ugen.sinOsc",
-	            freq: 1.0,
-	            mul: 0.25
-	        }
-	    }
+		synthDef: {
+			id: "carrier",
+			ugen: "flock.ugen.triOsc",
+			freq: {
+				ugen: "flock.ugen.value",
+				value: 440,
+				add: {
+					ugen: "flock.ugen.sin",
+					freq: 5,
+					mul: 100
+				}
+			},
+			mul: {
+				id: "mod",
+				ugen: "flock.ugen.sinOsc",
+				freq: 1.0,
+				mul: 0.25
+			}
+		}
 	});
-  synth.play();
+	synth.play();
 }
