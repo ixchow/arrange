892996a5a3b733aae98fb1b551bbec79b1d2fa0b
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-08-20 00:23:04 -0700

    Switch from flocking to timbre for audio synthesis
    http://mohayonao.github.io/timbre.js/

--- game/scene1.js
+++ game/scene1.js
@@ -7,26 +7,6 @@ exports.prototype.tick = function() {
 };
 
 exports.prototype.init = function() {
-	var synth = flock.synth({
-		synthDef: {
-			id: "carrier",
-			ugen: "flock.ugen.triOsc",
-			freq: {
-				ugen: "flock.ugen.value",
-				value: 440,
-				add: {
-					ugen: "flock.ugen.sin",
-					freq: 5,
-					mul: 100
-				}
-			},
-			mul: {
-				id: "mod",
-				ugen: "flock.ugen.sinOsc",
-				freq: 1.0,
-				mul: 0.25
-			}
-		}
-	});
-	synth.play();
+	var musicName = ['gymnopedie', 'khoomii'][Math.floor(Math.random()*2)];
+	music[musicName].start();
 }
