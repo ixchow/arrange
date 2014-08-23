5e6c793b501078cca7bf51bcf3ef7d7f7946cb48
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-08-23 16:09:04 -0700

    Mute setting is persisted

--- engine/music.js
+++ engine/music.js
@@ -1,21 +1,41 @@
 var currentAudio;
 var isMuted = false;
 
+function supports_html5_storage() {
+  try {
+    return 'localStorage' in window && window['localStorage'] !== null;
+  } catch (e) {
+    return false;
+  }
+}
+
+if (supports_html5_storage) {
+	isMuted = localStorage.getItem("mute");
+}
+
 exports = {
 	play: function(music, synth) {
 		currentAudio = music(synth);
-		currentAudio.start();
+		if (!isMuted) {
+			currentAudio.start();
+		}
 	},
 	isMuted: function() {
 		return isMuted;
 	},
 	mute: function() {
 		isMuted = true;
+		if (supports_html5_storage) {
+			isMuted = localStorage.setItem("mute", isMuted);
+		}
 		if (!currentAudio) return;
 		currentAudio.stop();
 	},
 	unmute: function() {
 		isMuted = false;
+		if (supports_html5_storage) {
+			isMuted = localStorage.setItem("mute", isMuted);
+		}
 		if (!currentAudio) return;
 		currentAudio.start();
 	},
