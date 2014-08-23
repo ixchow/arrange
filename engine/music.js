555de9d2609b5866c75c551ccf02f5fd9a759023
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-08-23 16:05:23 -0700

    Mute can toggle off and on

--- engine/music.js
+++ engine/music.js
@@ -1,16 +1,29 @@
 var currentAudio;
+var isMuted = false;
 
 exports = {
 	play: function(music, synth) {
 		currentAudio = music(synth);
 		currentAudio.start();
 	},
+	isMuted: function() {
+		return isMuted;
+	},
 	mute: function() {
+		isMuted = true;
 		if (!currentAudio) return;
 		currentAudio.stop();
 	},
 	unmute: function() {
+		isMuted = false;
 		if (!currentAudio) return;
 		currentAudio.start();
+	},
+	toggleMute: function() {
+		if (isMuted) {
+			this.unmute();
+		} else {
+			this.mute();
+		}
 	}
 };
