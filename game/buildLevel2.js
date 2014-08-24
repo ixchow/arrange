dc2ecc357bc68e84f295239124fd7b69e60f162d
Jim McCann <ix@tchow.com>
2014-08-24 01:34:27 -0400

    buildLevel2 flips the level around so it matches the text; path_test has some path parts in it for path consistency stuff

---
+++ game/buildLevel2.js
@@ -0,0 +1,32 @@
+exports = function(tileMap, txt) {
+	var groups = {};
+	var rows = txt.trim().split('\n');
+	var y = (rows.length / 2) | 0;
+	var xt = rows.map(function (yt) {
+		var x = -1;
+		--y;
+		return yt.split(' ').map(function(t) {
+			x++;
+			if (t == '..') return null;
+			if (!groups[t[1]]) {
+				groups[t[1]] = { tiles: [], r: 0 };
+			}
+			if (!groups[t[1]].at) {
+				groups[t[1]].at = { x: x, y: y };
+			}
+			var pt = { x: x - groups[t[1]].at.x, y: y - groups[t[1]].at.y };
+			var tile = tileMap[t[0]];
+			if (!tile) {
+				throw new Error(t);
+			}
+			groups[t[1]].tiles.push({ tile: tile.t, r: tile.r, at: pt});
+		});
+	});
+	
+	var newArray = []
+	for (var key in groups) {
+	    newArray.push(groups[key]);
+	}
+	
+	return newArray;
+};
---
+++ game/levels/path_test.js
@@ -0,0 +1,25 @@
+exports = function() {
+	var tileMap = {
+		">": { r:0, t: game.tiles.PathStart },
+		"^": { r:1, t: game.tiles.PathStart },
+		"<": { r:2, t: game.tiles.PathStart },
+		"v": { r:3, t: game.tiles.PathStart },
+		"D": { r:0, t: game.tiles.PathEnd },
+		"n": { r:1, t: game.tiles.PathEnd },
+		"C": { r:2, t: game.tiles.PathEnd },
+		"u": { r:3, t: game.tiles.PathEnd },
+		"-": { r:0, t: game.tiles.PathStraight },
+		"|": { r:1, t: game.tiles.PathStraight },
+		"J": { r:0, t: game.tiles.PathLeft },
+		",": { r:1, t: game.tiles.PathLeft },
+		"r": { r:2, t: game.tiles.PathLeft },
+		"L": { r:3, t: game.tiles.PathLeft },
+		"X": { r:0, t: game.tiles.Pillar },
+	};
+	var txt = "";
+	txt += ">1 -1 -1 ,1\n"
+	txt += "v2 D3 |4 J5\n"
+	txt += "J2 r7 u4 L6\n"
+
+	return game.buildLevel2(tileMap, txt);
+};
