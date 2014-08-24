19d7e03d96f056d7a3ca1319b2ae7772fadfe080
Jim McCann <ix@tchow.com>
2014-08-23 20:53:36 -0400

    Checking code for overlaps is running. Is it working? That's a different question.

--- game/tiles.js
+++ game/tiles.js
@@ -12,12 +12,47 @@ Tile.prototype.emit = function() {
 	this.mesh.emit();
 };
 
+//Directions are e,n,w,s
+var dir = {
+	e:0,
+	n:1,
+	w:2,
+	s:3,
+};
+var o = {
+	low: 1 << 0,
+	high:1 << 1,
+};
 exports = {
 	//Path tiles have an entrance to the west in their default orientation.
-	PathStart: new Tile({ mesh:'path_start' }),
-	PathEnd: new Tile({ mesh:'path_end' }),
-	PathStraight: new Tile({ mesh:'path_straight' }),
-	PathLeft: new Tile({ }),
-	PathRight: new Tile({ }),
-	Pillar: new Tile({ mesh:'pillar' })
+	PathStart: new Tile({
+		mesh:'path_start',
+		pathOut:dir.e,
+		needClear:o.low | o.high
+	}),
+	PathEnd: new Tile({
+		mesh:'path_end',
+		pathIn:dir.w,
+		needClear:o.low | o.high
+	}),
+	PathStraight: new Tile({
+		mesh:'path_straight',
+		pathIn:dir.w,
+		pathOut:dir.e,
+		needClear:o.low | o.high
+	}),
+	PathLeft: new Tile({
+		pathIn:dir.w,
+		pathOut:dir.n,
+		needClear:o.low | o.high
+	}),
+	PathRight: new Tile({
+		pathIn:dir.w,
+		pathOut:dir.s,
+		needClear:o.low | o.high
+	}),
+	Pillar: new Tile({
+		mesh:'pillar',
+		fill:o.low | o.high
+	})
 };
