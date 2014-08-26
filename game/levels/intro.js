27ee95410bafa492601156f787b90e0bd6aa5ee8
Jim McCann <ix@tchow.com>
2014-08-25 20:58:59 -0400

    some sort of intro

--- game/levels/intro.js
+++ game/levels/intro.js
@@ -1,30 +1,34 @@
 exports = function() {
 	var tileMap = {
 		"p": { r:0, t: game.tiles.SqueezePerson },
+		"e": { r:0, t: game.tiles.Empty },
 	};
 
 	var txt = "";
-	txt += ".. .. ..\n";
-	txt += ".. p. ..\n";
-	txt += ".. .. ..\n";
+	txt += "e. e. e.\n";
+	txt += "e. e. e.\n";
+	txt += "e. e. e.\n";
 
 	var level = game.buildLevel(tileMap, txt);
 
 	level.addScriptTriggers = function(arrange) {
 		arrange.scriptTriggers = [];
 		arrange.scriptTriggers.push({
-			at:{x:1,y:0},
+			at:{x:1,y:-1},
 			name:"intro",
 			advance:true,
 			script:{
 				pawn:{
-					mesh:meshes.characters.empty,
+					mesh:meshes.characters.pawn,
 					actions:[
-						{narrate:'FRAGMENTS<br/><span style="text-align:right">by avh4 and <a href="http://tchow.com">ix</a></span>'},
-						{appear:{x:0,y:1}},
+						{narrate:'FRAGMENTS<br/>by avh4 and ix</span>'},
+						{appear:{x:1,y:-1}},
 						{say:"I can't remember."},
 						{say:"I don't know why I'm here."},
-						{vanish:null}
+						{walk:[{x:0,y:-2}]},
+						{say:"Help me."},
+						{vanish:null},
+						{warp:'tut00'},
 					]
 				}
 			}
